abb1000eb8316977b742dbb4ca84c04d474dc176
OlgaVysochanska <olavysochanska@gmail.com>
2022-12-08 12:06:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { refs } from "./js/refs";
 import { fetchImages } from "./js/fetchImages";
 import { createGallery } from "./js/createGallery";
-import { PixabayAPI } from "./js/PixabayApi";
+import { PixabayAPI } from ".src/js/PixabayApi";
 
 const pixabayApi = new PixabayAPI();
 
